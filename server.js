dff75dac49b352b52ffdb86a8cb8c5408daac9b2
VasiliyMac <cartario@yandex.ru>
2020-10-11 23:10:58 +0300

    useReducer, socketEmit, new Map()

--- server.js
+++ server.js
@@ -3,18 +3,48 @@ const http = require("http");
 
 
 const app = express();
+app.use(express.json());
+
 const server = http.createServer(app);
 
 const io = require("socket.io")(server);
 
 const PORT = 9999;
 
+const rooms = new Map();
+
 app.get('/rooms', (req, res)=> {
-  res.send('Hello')
+  res.json(rooms);
+});
+
+app.post('/rooms', (req, res)=>{
+  const {roomId, username} = req.body;
+
+  if(!rooms.has(roomId)){
+    rooms.set(roomId, new Map([
+      ['users', new Map()],
+      ['messages', []]
+    ]));
+    
+  }
+  
+  res.json({
+    status: 'success',
+    data: [...rooms.keys()]
+  }); 
+  
+  
 });
 
 io.on('connection', (socket)=>{
-  console.log('user connsected', socket.id)
+  socket.on('ROOM:JOIN', (data) => {
+    socket.join(data.roomId);
+    const user = rooms.get(data.roomId).get('users').set(socket.id, data.username);
+    const users = rooms.get(data.roomId).get('users').values();
+    socket.to(data.roomId).broadcast.emit('ROOM:JOINED', users);
+    
+  });
+  
 })
 
 server.listen(PORT, (err)=>{
--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 import socket from '../src/socket';
 
-function App() {
-  const [data, setValue] = React.useState({roomId: '', name: ''});
-  const {name, roomId} = data;
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'JOINED':
+      return {...state, joined: true, roomId: action.payload.roomId, username: action.payload.username}
+    default:
+      return state;
+  }
+};
 
-  const handlerSubmit = (e) => {
-    e.preventDefault();    
+const initialState = {
+  joined: false,
+  roomId: null,
+  username: null,
+};
+
+function App() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const [data, setValue] = React.useState({roomId: '', username: ''});
+  const {roomId, username} = data;
+
+  const onLogin = () => {
+    dispatch({
+      type: 'JOINED', 
+      payload: data
+    });
+
+    socket.emit('ROOM:JOIN', data);
+  };  
+  
+  const handlerSubmit = (e) => {    
+    e.preventDefault();
+    axios.post('/rooms', data).then(onLogin);
   };
 
   const handlerChange = (e) => {
@@ -18,32 +45,50 @@ function App() {
     });     
   };
 
+  React.useEffect(()=>{
+    socket.on('ROOM:JOINED', (users)=>{
+      console.log(users);
+    });
+  }, []);
+console.log(state)
   return (
     <div className="App">
       <div className="container">
+
+        {!state.joined ? 
         <form className="form" onSubmit={handlerSubmit}>
-        <div className="inputBox">
-          <input className="input roomId"
-            name="roomId"
-            id="roomId"
-            type="text"
-            placeholder="roomID"
-            value={roomId}
-            onChange={handlerChange}
-            />
-        </div>
-        <div className="inputBox">
-          <input className="input name"
-            name="name"
-            id="name"
-            type="text"
-            placeholder="name"
-            value={name}
-            onChange={handlerChange}
-            />
-        </div>       
-        <button disabled={!name&!roomId} className="button" type="submit">ВОЙТИ</button>
-      </form>
+          <div className="inputBox">
+            <input className="input roomId"
+              name="roomId"
+              id="roomId"
+              type="text"
+              placeholder="roomID"
+              value={roomId}
+              onChange={handlerChange}
+              />
+          </div>
+          <div className="inputBox">
+            <input className="input name"
+              name="username"
+              id="name"
+              type="text"
+              placeholder="name"
+              value={username}
+              onChange={handlerChange}
+              />
+          </div>       
+          <button disabled={!username&!roomId} className="button" type="submit">ВОЙТИ</button>
+        </form>
+        :   
+
+        <div className="authorized">
+          <h1>Authorized</h1>
+          <p>Room: {roomId}</p>
+          <p>User: {username}</p>
+        </div> 
+
+        }
+
       </div>
     </div>
   );
